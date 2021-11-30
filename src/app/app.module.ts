72a35b748dd27a83072cf1ebeab742ee550681b2
Guroz <v.alessandro.developer@gmail.com>
2021-11-30 15:45:56 +0100

    navbar + first part routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BeersService } from './beers.service';
 import { BeersComponent } from './beers/beers.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { BeerComponent } from './beer/beer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -13,8 +14,24 @@ import { UsersComponent } from './users/users.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserComponent } from './user/user.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'users'
+  },
+  {
+    path: 'users',
+    pathMatch: 'full',
+    component: UserComponent
+  },
+  {
+    path: 'users/new',
+    component: UserComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -23,7 +40,8 @@ import { UserDetailComponent } from './user-detail/user-detail.component';
     BeerComponent,
     UsersComponent,
     UserComponent,
-    UserDetailComponent
+    UserDetailComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +49,8 @@ import { UserDetailComponent } from './user-detail/user-detail.component';
     FontAwesomeModule,
     FormsModule,
     NgxPaginationModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
